8ce5801d5358434ad5f536112d33d1ab8752ce22
1abhisheknigam <abhisheknigam@live.com>
2016-11-06 17:07:38 -0500

    added basic button functionality

---
+++ Assets/Scripts/clickListeners.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class clickListeners : MonoBehaviour {
+
+    public void OnPlayClick()
+    {
+        Debug.Log("You have clicked the Play button!");
+    }
+
+    public void OnCreditsClick()
+    {
+        Debug.Log("You have clicked the Credits button!");
+    }
+
+    public void OnQuitClick()
+    {
+        Debug.Log("You have clicked the Quit button!");
+    }
+}
