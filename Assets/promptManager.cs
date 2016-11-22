49eebc45279bba8e7716199e12f2fdbfa1c0db11
1abhisheknigam <abhisheknigam@live.com>
2016-11-21 22:01:47 -0500

    modified prompts for better visibility

--- Assets/promptManager.cs
+++ Assets/promptManager.cs
@@ -7,9 +7,9 @@ public class promptManager : MonoBehaviour {
     public Text promptTextElem;
 
     private static string interact_message = "Press the Interact button to activate!";
-    private static string loot_incomplete_message = "You haven't collected enough loot to escape yet";
-    private static string loot_partial_message = "You have enough loot to escape! Or, try to collect them all!";
-    private static string loot_complete_message = "You collected all the loot! Try to escape alive!";
+    private static string loot_incomplete_message = "You haven't collected enough loot to escape yet!";
+    private static string loot_partial_message = "You have collected enough loot to escape! Escape to the elevator, or collect them all!";
+    private static string loot_complete_message = "You collected all the loot! Try to escape to the elevator exit alive!";
     // Use this for initialization
     void Start () {
 	}
