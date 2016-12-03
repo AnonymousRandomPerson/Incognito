401315d78a7eab6186d4838e4e0bab7c79690391
1abhisheknigam <abhisheknigam@live.com>
2016-12-03 16:05:58 -0500

    added help screen in pause menu

--- Assets/Scripts/UI/pauseManager.cs
+++ Assets/Scripts/UI/pauseManager.cs
@@ -2,15 +2,19 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class pauseManager : MonoBehaviour {
     public Canvas pauseCanvas;
     bool paused;
+
+    public Canvas helpCanvas;
+    public RawImage pc, controller;
 	// Use this for initialization
 	void Start () {
         paused = false;
         pauseCanvas.enabled = false;
-	}
+    }
 	
 	// Update is called once per frame
     void Update () {
@@ -18,18 +22,28 @@ public class pauseManager : MonoBehaviour {
             OnPauseQuitClick();
         }
         if (Input.GetButtonDown("Pause")) {
-            if (!paused)
+            if (helpCanvas.enabled)
             {
-                Time.timeScale = 0;
-                pauseCanvas.enabled = true;
+                helpCanvas.enabled = false;
+                helpCanvas.GetComponent<CanvasGroup>().interactable = false;
+                pauseCanvas.GetComponent<CanvasGroup>().interactable = true;
                 EventSystem.current.SetSelectedGameObject(pauseCanvas.GetComponentInChildren<UnityEngine.UI.Button>().gameObject);
             }
             else
             {
-                Time.timeScale = 1;
-                pauseCanvas.enabled = false;
+                if (!paused)
+                {
+                    Time.timeScale = 0;
+                    pauseCanvas.enabled = true;
+                    EventSystem.current.SetSelectedGameObject(pauseCanvas.GetComponentInChildren<UnityEngine.UI.Button>().gameObject);
+                }
+                else
+                {
+                    Time.timeScale = 1;
+                    pauseCanvas.enabled = false;
+                }
+                paused = !paused;
             }
-            paused = !paused;
         }
 	}
 
@@ -43,5 +57,26 @@ public class pauseManager : MonoBehaviour {
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnHelpClick()
+    {
+        Debug.Log("Presed help");
+        helpCanvas.enabled = true;
+        helpCanvas.GetComponent<CanvasGroup>().interactable = true;
+        pauseCanvas.GetComponent<CanvasGroup>().interactable = false;
+        EventSystem.current.SetSelectedGameObject(helpCanvas.GetComponentInChildren<UnityEngine.UI.Button>().gameObject);
+    }
+
+    public void OnHelpPCClick()
+    {
+        pc.enabled = true;
+        controller.enabled = false;
+    }
+
+    public void OnHelpControllerClick()
+    {
+        pc.enabled = false;
+        controller.enabled = true;
     }
 }
